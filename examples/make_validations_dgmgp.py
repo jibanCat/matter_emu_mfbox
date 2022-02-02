587a229145a2ce30bed6eb49e8ec57c6659ae818
jibancat <mho026@ucr.edu>
2022-02-02 00:43:31 -0800

    init commit

---
+++ examples/make_validations_dgmgp.py
@@ -0,0 +1,174 @@
+import os
+import json
+
+from examples.make_validations import *
+
+from matter_multi_fidelity_emu.gpemulator_singlebin import SingleBindGMGP
+
+# set a random number seed to reproducibility
+np.random.seed(0)
+
+matplotlib.use("pdf")
+
+
+def generate_data(
+        folder_1: str = "data/50_LR_3_HR_box_512_combined_128res",
+        folder_2: str = "data/50_LR_box_300_3_HR_box_512_combined_128res",
+    ):
+    data_1 = PowerSpecs(folder=folder_1)
+    data_2 = PowerSpecs(folder=folder_2)
+    return data_1, data_2
+
+
+
+def do_validations(
+    folder_1: str = "data/50_LR_3_HR_box_512_combined_128res",
+    folder_2: str = "data/50_LR_box_300_3_HR_box_512_combined_128res",
+    n_optimization_restarts: int = 30,
+    n_fidelities: int = 2,
+    output_folder: str = "output/dGMGP_50_dmonly128_m1mpc512_m2mpc300_3_dmonly512",
+    ARD_last_fidelity: bool = False,
+):
+    """
+    Train and test models, and plot
+    1. predicted / exact power spectrum
+    2. absolute error plot
+    3. parameter plots
+
+    Only support 2 fidelities now.
+
+    Parameters:
+    ----
+    folder: the folder contains the the training and testing data. See data/50_LR_3_HR
+        for example.
+    n_optimization_restarts: number of optimization you want to repeat. The GPy will
+        choose the best hyperparameters among those repetitions. More is better.
+    n_fidelities: only supports 2 now. You may try a larger number but some tweaks might
+        be needed.
+    turn_off_bias_nargp: not adding bias kernel for NARGP in high-fidelity. In case you
+        find the optimization result is not stable, try turning off bias kernel. Some time
+        the training data at high-fidelity is not enough to train the bias kernel and
+        induce some unstable predictions.
+    ARD_last_fidelity: whether to apply ARD for the last (highest) fidelity.
+        Default, False.
+    """
+    # create output folder, recursively
+    os.makedirs(output_folder, exist_ok=True)
+    old_dir = os.getcwd()
+    print("Current path:", old_dir)
+
+    # get training and testing data. Normalization included.
+    data_1, data_2 = generate_data(folder_1=folder_1, folder_2=folder_2)
+
+    import pdb
+    pdb.set_trace()
+
+    # change path for saving figures
+    os.chdir(output_folder)
+    print(">> ", os.getcwd())
+
+    # Multi-fidelity
+    # linear multi-fidelity
+    dgmgp = SingleBindGMGP(
+
+    )
+
+    # Data (M1, M2, H)
+    dgmgp = SingleBindGMGP(
+        [data_1.X_train_norm[0], data_2.X_train_norm[0], data_1.X_train_norm[1]],
+        [data_1.Y_train_norm[0], data_2.Y_train_norm[0], data_1.Y_train_norm[1]],
+        n_fidelities=n_fidelities,
+        n_samples=500,
+        optimization_restarts=n_optimization_restarts,
+        ARD_last_fidelity=ARD_last_fidelity,
+    )
+
+    # Single-fidelity
+    # high-fidelity only emulator
+    hf_only = SingleBinGP(data_1.X_train_norm[-1], data_1.Y_train[-1])
+    lf_only_1 = SingleBinGP(data_1.X_train_norm[0], data_1.Y_train[0])
+    lf_only_2 = SingleBinGP(data_2.X_train_norm[0], data_2.Y_train[0])
+
+    # optimize each model
+    hf_only.optimize_restarts(n_optimization_restarts=n_optimization_restarts)
+    lf_only_1.optimize_restarts(n_optimization_restarts=n_optimization_restarts)
+    lf_only_2.optimize_restarts(n_optimization_restarts=n_optimization_restarts)
+
+    # testing set
+    means_dgmgp, vars_dgmgp, pred_exacts_dgmgp = validate_mf(data_1, model=dgmgp)
+    means_hfonly, vars_hfonly, pred_exacts_hfonly = validate_sf(data_1, model=hf_only)
+    means_lfonly_1, vars_lfonly_1, pred_exacts_lfonly_1 = validate_sf(data_1, model=lf_only_1)
+    means_lfonly_2, vars_lfonly_2, pred_exacts_lfonly_2 = validate_sf(data_1, model=lf_only_2)
+
+    # versus HF
+    do_emulator_error_plots(
+        data_1,
+        means_dgmgp,
+        means_hfonly,
+        pred_exacts_dgmgp,
+        pred_exacts_hfonly,
+        label_mf="dGMGP",
+        label_sf="HF only",
+        figure_name="dGMGP",
+    )
+    do_emulator_error_plots(
+        data_1,
+        means_dgmgp,
+        means_lfonly_1,
+        pred_exacts_dgmgp,
+        pred_exacts_lfonly_1,
+        label_mf="dGMGP",
+        label_sf="LF only (1)",
+        figure_name="dGMGP_lf1",
+    )
+    # versus LF
+    do_emulator_error_plots(
+        data_1,
+        means_dgmgp,
+        means_lfonly_2,
+        pred_exacts_dgmgp,
+        pred_exacts_lfonly_2,
+        label_mf="dGMGP",
+        label_sf="LF only (2)",
+        figure_name="dGMGP_lf2",
+    )
+
+    # pred/exact plot
+    do_pred_exact(data_1, means_dgmgp, pred_exacts_dgmgp, label_mf="dGMGP", figure_name="dGMGP")
+
+    # # saving hyperparameters
+    # with open("ar1.json", "w") as f:
+    #     json.dump(ar1.to_dict(), f, indent=2)
+
+    # with open("nargp.json", "w") as f:
+    #     json.dump(nargp.to_dict(), f, indent=2)
+
+    # with open("hf_only.json", "w") as f:
+    #     json.dump(hf_only.to_dict(), f, indent=2)
+
+    # with open("lf_only.json", "w") as f:
+    #     json.dump(lf_only.to_dict(), f, indent=2)
+
+    # saving AR1
+    os.makedirs("dGMGP/", exist_ok=True)
+
+    np.savetxt(os.path.join("dGMGP", "all_gp_mean"), np.array(means_dgmgp))
+    np.savetxt(os.path.join("dGMGP", "all_gp_var"), np.array(vars_dgmgp))
+    np.savetxt(os.path.join("dGMGP", "pred_exacts"), np.array(pred_exacts_dgmgp))
+    np.savetxt(os.path.join("dGMGP", "all_true"), np.array(data_1.Y_test[0]))
+    np.savetxt(os.path.join("dGMGP", "kf"), np.array(data_1.kf))
+    # [HF] also save the predictions from hf-only
+    np.savetxt(os.path.join("dGMGP", "all_hf_gp_mean"), np.array(means_hfonly))
+    np.savetxt(os.path.join("dGMGP", "all_hf_gp_var"), np.array(vars_hfonly))
+    np.savetxt(os.path.join("dGMGP", "pred_exacts_hf"), np.array(pred_exacts_hfonly))
+    # [LF] also save the predictions from lf-only
+    np.savetxt(os.path.join("dGMGP", "all_lf_gp_mean_1"), np.array(means_lfonly_1))
+    np.savetxt(os.path.join("dGMGP", "all_lf_gp_var_1"), np.array(vars_lfonly_1))
+    np.savetxt(os.path.join("dGMGP", "pred_exacts_lf_1"), np.array(pred_exacts_lfonly_1))
+
+    np.savetxt(os.path.join("dGMGP", "all_lf_gp_mean_2"), np.array(means_lfonly_2))
+    np.savetxt(os.path.join("dGMGP", "all_lf_gp_var_2"), np.array(vars_lfonly_2))
+    np.savetxt(os.path.join("dGMGP", "pred_exacts_lf_2"), np.array(pred_exacts_lfonly_2))
+
+    # back to root folder
+    os.chdir(old_dir)
