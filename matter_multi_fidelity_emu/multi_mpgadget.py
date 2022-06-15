1058b250fe10dd10b513c28d836fb32ca85d47f2
jibancat <mho026@ucr.edu>
2022-06-14 22:20:47 -0700

    across redshifts

--- matter_multi_fidelity_emu/multi_mpgadget.py
+++ matter_multi_fidelity_emu/multi_mpgadget.py
@@ -2,7 +2,7 @@
 Loading the total mass power spectrum from default
 MP-Gadget simulations.
 """
-from SimulationRunner.multi_sims import MultiPowerSpec, PowerSpec
+from .multi_sims import MultiPowerSpec, PowerSpec
 from typing import Tuple, List, Optional, Generator
 
 import os
@@ -95,15 +95,15 @@ class MPGadgetPowerSpec(PowerSpec):
     """
 
     def __init__(self,
-        submission_dir: str = "test/", z0 : float = 0.0,
+        submission_dir: str = "test/", scale_factor : float = 1.0,
     ) -> None:
         super(PowerSpec, self).__init__(submission_dir)
 
         # read into arrays
         # Matter power specs from simulations
-        kk, pk, modes = self.read_powerspec(submission_dir=submission_dir, z0=z0)
+        kk, pk, modes = self.read_powerspec(submission_dir=submission_dir, scale_factor=scale_factor)
 
-        self._scale_factors = np.array([1 / (1 + z0)])
+        self._scale_factor = scale_factor
 
         self._k0 = kk
         self._powerspecs = pk
@@ -146,7 +146,7 @@ class MPGadgetPowerSpec(PowerSpec):
         """
         return self._k0
 
-    def read_powerspec(self, submission_dir: str, z0: float) -> Tuple[np.ndarray, np.ndarray]:
+    def read_powerspec(self, submission_dir: str, scale_factor: float) -> Tuple[np.ndarray, np.ndarray]:
         """
         Read power spectrum from a output folder
 
@@ -154,9 +154,6 @@ class MPGadgetPowerSpec(PowerSpec):
         ----
         z0 : the redshift of the power spectrum you want to load.
         """
-        tol = 1e-4 # tolerance
-        # the scale factor you condition on
-        scale_factor = 1 / (1 + z0)
 
         # the maximum k is controlled by Ng
         kk, pk, modes = load_total_mass(submission_dir, scale_factor=scale_factor)
@@ -172,28 +169,40 @@ class MultiMPGadgetPowerSpec(MultiPowerSpec):
     """
     Output a HDF5 file
 
-    Interpolate the LowRes (if necessary).
+    This class tries to convert the powerspec data (across redshifts) output from MP-Gadget to a single h5 file.
+    This meant to be similar to the routines in sbird.lya_emulator_full.
+
+    Parameters:
+    ----
+    all_submission_dirs : a List of paths to the MP-Gadget simulation submission folders.
+    Latin_json : path to the Latin hypercube json file.
+    selected_ind : select a subset of LHD to generate the h5 file.
     """
 
-    def __init__(self, all_submission_dirs: List[str], Latin_json: str, selected_ind: Optional[np.ndarray],
-        z0 : float = 0.0,) -> None:
+    def __init__(
+            self,
+            all_submission_dirs: List[str],
+            Latin_json: str,
+            selected_ind: Optional[np.ndarray],
+            scale_factors : List[float] = [1.0000, 0.8333, 0.6667, 0.5000, 0.3333, 0.2500],
+        ) -> None:
         super().__init__(all_submission_dirs, Latin_json=Latin_json, selected_ind=selected_ind)
 
         # assign attrs for loading MPGadget power specs
-        self.z0 = z0
+        self.scale_factors = scale_factors
 
     def create_hdf5(self, hdf5_name: str = "MultiMPGadgetPowerSpec.hdf5") -> None:
 
         """
         - Create a HDF5 file for powerspecs from multiple simulations.
-        - Each simulation stored in subgroup, includeing powerspecs and
-        camb linear power specs.
-        - Each subgroup has their own simulation parameters extracted from
-        SimulationICs.json to reproduce this simulation.
-        - Parameters from Latin HyperCube sampling stored in upper group level,
+        - Parameters from Latin HyperCube sampling stored in first layer,
         the order of the sampling is the same as the order of simulations.
-
+        - Powerspecs stored as a large array in the first layer.
+        - Stores redshift and scale factor arrays
+        - Store k bins.
         TODO: add a method to append new simulations to a created hdf5.
+        <KeysViewHDF5 ['flux_vectors', 'kfkms', 'kfmpc', 'params', 'zout']>
+
         """
         # open a hdf5 file to store simulations
         with h5py.File(hdf5_name, "w") as f:
@@ -203,33 +212,69 @@ class MultiMPGadgetPowerSpec(MultiPowerSpec):
             for key, val in self.Latin_dict.items():
                 f.create_dataset(key, data=val)
 
-            # using generator to iterate through simulations,
-            # PowerSpec stores big arrays so we don't want to load
-            # everything to memory
-            for i, ps in enumerate(self.load_PowerSpecs(self.all_submission_dirs)):
-                sim = f.create_group("simulation_{}".format(i))
+            # Make input parameter column
+            parameter_names = self.Latin_dict["parameter_names"]
+            # (num of sims, num of parameters)
+            params = np.full(
+                (
+                    len(self.Latin_dict[parameter_names[0]]),
+                    len(parameter_names),
+                ),
+                fill_value=np.nan,
+            )
+            for ith_param, pname in enumerate(parameter_names):
+                params[:, ith_param] = self.Latin_dict[pname]
+            assert np.sum(np.isnan(params)) < 1
+            f.create_dataset("params", data=params)
 
-                # store arrays to sim subgroup
-                # Simulation Power spectra:
-                sim.create_dataset("scale_factors", data=np.array(ps.scale_factors))
-                sim.create_dataset("powerspecs", data=ps.powerspecs)
-                sim.create_dataset("k0", data=ps.k0)
+            f.create_dataset("scale_factors", data=np.array(self.scale_factors))
+            f.create_dataset("zout", data=1 / np.array(self.scale_factors) - 1)
 
-                # for calculating the uncertainty
-                sim.create_dataset("modes", data=ps.modes)
-                sim.create_dataset("poisson_noise", data=ps.poisson_noise)
+            # Get the shape first
+            # TODO: might be a better way append arrays
+            ps_test = MPGadgetPowerSpec(submission_dir, scale_factor) # Note: there are default rebinning parameters
+            
+            kk      = ps_test.k0                                      # Note: assumption is all k bins are the same
+            ps_size = ps_test.powerspecs.shape[0]
+            mode_size = ps_test.modes.shape[0]
+            assert kk.shape[0] == ps_size
+            assert mode_size == ps_size
 
-                sim.create_dataset("camb_redshifts", data=np.array(ps.camb_redshifts))
-                sim.create_dataset("camb_matters", data=ps.camb_matters)
+            # (num of simulations, num of redshifts, num of k bins)
+            sim_powerspecs = np.full(
+                (
+                    len(self.all_submission_dirs),
+                    len(self.scale_factors),
+                    ps_size,
+                ),
+                fill_value=np.nan,
+            )
+            sim_modes = np.full(
+                (
+                    len(self.all_submission_dirs),
+                    len(self.scale_factors),
+                    ps_size,
+                ),
+                fill_value=np.nan,
+            )
 
-                # stores param json to metadata attrs
-                for key, val in ps.param_dict.items():
-                    sim.attrs[key] = val
+            f.create_dataset("kfmpc", data=np.array(kk))
+
+            for ith_sim, submission_dir in enumerate(self.all_submission_dirs):
+                for jth_red, scale_factor in enumerate(self.scale_factors):
+                    ps = MPGadgetPowerSpec(submission_dir, scale_factor)
+
+                    sim_powerspecs[ith_sim, jth_red, :] = ps.powerspecs
+                    sim_modes[ith_sim, jth_red, :]      = ps.modes
+
+            assert np.sum(np.isnan(sim_powerspecs)) < 1
+            f.create_dataset("powerspecs", data=sim_powerspecs)
+            f.create_dataset("modes", data=sim_modes)
 
     @staticmethod
-    def load_PowerSpecs(all_submission_dirs: List[str]) -> Generator:
+    def load_PowerSpecs(all_submission_dirs: List[str], scale_factor: float) -> Generator:
         """
         Iteratively load the PowerSpec class
         """
         for submission_dir in all_submission_dirs:
-            yield MPGadgetPowerSpec(submission_dir)
+            yield MPGadgetPowerSpec(submission_dir, scale_factor)
