a2bacf84f39ac611204fd2b4a6c21899a1923bd8
jibanmac <mho026@ucr.edu>
2023-03-21 20:30:14 -0700

    2023.03.21 : these are half done

--- matter_multi_fidelity_emu/multi_haloes.py
+++ matter_multi_fidelity_emu/multi_haloes.py
@@ -1,169 +1,131 @@
 """
 Compute halo mass functions and save into a HDF5 file
 """
-from typing import Generator, List
+from typing import Generator, List, Optional
+
+from .multi_sims import MultiPowerSpec
 
 import os
 import numpy as np
+import h5py
 import re
 
 from .multi_sims import GadgetLoad
 from .hmffromfof import HMFFromFOF
 
-class MPISubmit(object):
+
+class MultiHMF(MultiPowerSpec):
 
     """
-    Read the mpi_submit, the sbatch submission for MP-Gadget
-    into a class.
+    Output a HDF5 file
+
+    This class tries to convert the HMF data (across redshifts) output from MP-Gadget to a single h5 file.
+    This meant to be similar to the routines in sbird.lya_emulator_full.
+
+    Parameters:
+    ----
+    all_submission_dirs : a List of paths to the MP-Gadget simulation submission folders.
+    Latin_json : path to the Latin hypercube json file.
+    selected_ind : select a subset of LHD to generate the h5 file.
     """
 
-    def __init__(self, filename: str, gadget_dir: str = "~/codes/MP-Gadget/"):
-        self.filename = filename
-        self.gadget_dir = gadget_dir
-
-        with open(self.filename, "r") as f:
-            self.txt = f.readlines()
-
-
-    def get_basic_setups(self):
-        """
-        Return the lines without `mpirun` - this (ideally) includes
-        - SBATCH parameters
-        - module loads
-        - exports
-        """
-        # only check the 1st position in a line;
-        # this is because sbatch job's name could have mpirun
-        exclusion_bag = {"mpirun"}
-
-        basic_txt = []
-
-        for line in self.txt:
-            words = line.split()
-
-            if np.any([words[0] == exclusion_word for exclusion_word in exclusion_bag]):
-                continue
-    
-            basic_txt.append(line)
-
-        return basic_txt
-
-    def make_simulation_foftable(self, snapshots: List[int], mpi_submit_file: str = "mpi_submit_foftables", mpgadget_param_file: str = "mpgadget.param") -> None:
-        """
-        Generate a submission file for making the fof table (PART -> PIG)
-        """
-        basic_txt = self.get_basic_setups()
-
-        # snapshots could be more than one
-        for snapshot in snapshots:
-            basic_txt.append(self.mpirun_foftable(snapshot, mpgadget_param_file))
-
-        with open(mpi_submit_file, "w") as f:
-            f.write("".join(basic_txt))
-
-
-    def mpirun_foftable(self, snapshot: int, mpgadget_param_file: str = "mpgadget.param") -> str:
-        """
-        The line to make foftable from PART snapshot       
-        """
-        return "mpirun --map-by core {} {} 3 {}\n".format(self.gadget_dir, mpgadget_param_file, snapshot)
-
-class HaloMassFunction(GadgetLoad):
-
-    """
-    Compute the halo mass functions from foftable using MP-Gadget's
-    tool, HMFFromFOF (TODO: binning scheme might need to adjust,
-    https://jakevdp.github.io/blog/2012/09/12/dynamic-programming-in-python/)
-
-    There was a bug before that FOF tables were not generated from
-    the PART during the run time. Generate a script to get FOF tables
-    out of PART.
-
-    Example:
-    ./MP-Gadget paramfile.gadget 3 $snapnum
-    """
-
-    def __init__(self, submission_dir: str = "test/", gadget_dir: str = "~/codes/MP-Gadget/") -> None:
-        super(HaloMassFunction, self).__init__(submission_dir)
-
-        self.gadget_dir = gadget_dir
-
-        # acquire the current PART files
-        self._parts = sorted(self.get_PART_array())
-        # acquire the current PIG files
-        self._pigs = sorted(self.get_PIG_array())
-
-        # read mpi_submit into a class
-        # keep all SBATCH parameters the same;
-        # keep all module load the same
-        # make the mpirun free parameter
-        self.mpi_submit = MPISubmit(
-            os.path.join(self.submission_dir, "mpi_submit"), self.gadget_dir
+    def __init__(
+        self,
+        all_submission_dirs: List[str],
+        Latin_json: str,
+        selected_ind: Optional[np.ndarray],
+        scale_factors: List[float] = [1.0000, 0.8333, 0.6667, 0.5000, 0.3333, 0.2500],
+    ) -> None:
+        super().__init__(
+            all_submission_dirs, Latin_json=Latin_json, selected_ind=selected_ind
         )
 
-        # check # of FOF tables == # of snapshots
-        self._pigs_to_run = set(self._parts) - set(self._pigs)
-        if len(self._pigs_to_run) > 0:
-            print("[Warning] Some snapshots lack of FOF tables, recommend do self.make_foftables.")
+        # assign attrs for loading MPGadget power specs
+        self.scale_factors = scale_factors
 
-    def read_hmf(self):
+        self.selected_ind = selected_ind
+
+    def create_hdf5(self, hdf5_name: str = "MultiHMF.hdf5") -> None:
+
         """
-        Compute halo mass functions from PIG/ and read them into memory
+        - Create a HDF5 file for hmfs from multiple simulations.
+        - Parameters from Latin HyperCube sampling stored in first layer,
+        the order of the sampling is the same as the order of simulations.
+        - HMFs stored as a large array in the first layer.
+        - Stores redshift and scale factor arrays
+        - Store k bins.
+        TODO: add a method to append new simulations to a created hdf5.
+        <KeysViewHDF5 ['flux_vectors', 'kfkms', 'kfmpc', 'params', 'zout']>
+
         """
-        # checking if you need to re-generate FOF tables
-        assert len(self._parts) == len(self._pigs)
-        # checking if all snaphots in the table are available
-        if len(self.snapshots[:, 0]) > len(self._parts):
-            print("[Warning] missing snaphots ...")
-            print("... recommend rerun {}".format( set(self.snapshots[:, 1]) - set(self._parts) ))
+        # open a hdf5 file to store simulations
+        with h5py.File(hdf5_name, "w") as f:
+            # Save the selected ind for comparing with LF
+            if self.selected_ind is not None:
+                f.create_dataset("selected_ind", data=np.array(self.selected_ind))
 
-        # snapshot table records all PIG need to read
-        # snapshot table: | No. of snapshot | scale factor |
-        
-        # Note: here we only load all available PIGs, instead of all PIGs on the table
-        self.scale_factors = self.snapshots[:, 1][self._pigs]
+            # store the sampling from Latin Hyper cube dict into datasets:
+            # since the sampling size should be arbitrary, we should use
+            # datasets instead of attrs to stores these sampling arrays
+            for key, val in self.Latin_dict.items():
+                f.create_dataset(key, data=val)
 
-        foftable = lambda snapshot : "PIG_{}".format(snapshot)
+            # Make input parameter column
+            parameter_names = self.Latin_dict["parameter_names"]
+            # (num of sims, num of parameters)
+            params = np.full(
+                (len(self.Latin_dict[parameter_names[0]]), len(parameter_names),),
+                fill_value=np.nan,
+            )
+            for ith_param, pname in enumerate(parameter_names):
+                params[:, ith_param] = self.Latin_dict[pname]
+            assert np.sum(np.isnan(params)) < 1
+            f.create_dataset("params", data=params)
 
-        for i in self._pigs:
-            HMFFromFOF(foftable=foftable(i), h0=False, bins="auto")
+            f.create_dataset("scale_factors", data=np.array(self.scale_factors))
+            f.create_dataset("zout", data=1 / np.array(self.scale_factors) - 1)
 
+            # Get the shape first
+            # TODO: might be a better way append arrays
+            ps_test = MPGadgetPowerSpec(
+                self.all_submission_dirs[0], self.scale_factors[0]
+            )  # Note: there are default rebinning parameters
 
-    def make_foftables(self, mpgadget_param_file: str = "mpgadget.param"):
+            kk = ps_test.k0  # Note: assumption is all k bins are the same
+            ps_size = ps_test.powerspecs.shape[0]
+            mode_size = ps_test.modes.shape[0]
+            assert kk.shape[0] == ps_size
+            assert mode_size == ps_size
+
+            # (num of simulations, num of redshifts, num of k bins)
+            sim_powerspecs = np.full(
+                (len(self.all_submission_dirs), len(self.scale_factors), ps_size,),
+                fill_value=np.nan,
+            )
+            sim_modes = np.full(
+                (len(self.all_submission_dirs), len(self.scale_factors), ps_size,),
+                fill_value=np.nan,
+            )
+
+            f.create_dataset("kfmpc", data=np.array(kk))
+
+            for ith_sim, submission_dir in enumerate(self.all_submission_dirs):
+                for jth_red, scale_factor in enumerate(self.scale_factors):
+                    ps = MPGadgetPowerSpec(submission_dir, scale_factor)
+
+                    sim_powerspecs[ith_sim, jth_red, :] = ps.powerspecs
+                    sim_modes[ith_sim, jth_red, :] = ps.modes
+
+            assert np.sum(np.isnan(sim_powerspecs)) < 1
+            f.create_dataset("powerspecs", data=sim_powerspecs)
+            f.create_dataset("modes", data=sim_modes)
+
+    @staticmethod
+    def load_PowerSpecs(
+        all_submission_dirs: List[str], scale_factor: float
+    ) -> Generator:
         """
-        Make FOF tables from PART.
-
-        After generating the sh files, go to the submission folder and submit the .sh file.
+        Iteratively load the PowerSpec class
         """
-        self.mpi_submit.make_simulation_foftable(
-            self._pigs_to_run, os.path.join(self.submission_dir, "mpi_submit_foftables", mpgadget_param_file)
-        )
-
-
-
-    def get_PART_array(self) -> Generator:
-        """
-        Get indexes for PART snapshot folders
-        """
-        for f in self._outputfiles:
-            out = re.findall(r"PART_([0-9]*)", f)
-
-            # if matched
-            if len(out) == 1:
-                yield int(out[0])
-    
-    def get_PIG_array(self) -> Generator:
-        """
-        Get indexes for PIG snapshot folders
-        """
-        for f in self._outputfiles:
-            out = re.findall(r"PIG_([0-9]*)", f)
-
-            # if matched
-            if len(out) == 1:
-                yield int(out[0])
-    
-
-
-
-
+        for submission_dir in all_submission_dirs:
+            yield MPGadgetPowerSpec(submission_dir, scale_factor)
